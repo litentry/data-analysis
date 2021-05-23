882ef13b0a10180a63ef4f08f5a973a26a3ac150
Cui Qin <qincuidhu@gmail.com>
2021-05-23 21:26:18 +0200

    add a common mapping

---
+++ nft-scanner/src/mapping.ts
@@ -0,0 +1,29 @@
+import { Transfer, ERC721 } from '../generated/Zedhorse/ERC721'
+import { NftOwner, NftBalance, TransferTrace } from '../generated/schema'
+
+export function handleTransfer(event: Transfer): void {
+    let id = event.address.toHex() + '#' + event.params.tokenId.toHex()
+    let nftOwner = NftOwner.load(id)
+    if (nftOwner == null) {
+        nftOwner = new NftOwner(id)
+    }
+    nftOwner.tokenId = event.params.tokenId
+    nftOwner.owner = event.params.to
+    nftOwner.contract = event.address
+    nftOwner.save()
+    
+    //update the amount ot the token 
+    let contract = ERC721.bind(event.address)
+    let nftBalance = new NftBalance(event.params.to.toHex())
+    nftBalance.amount = contract.balanceOf(event.params.to)
+    nftBalance.totalSupply = contract.totalSupply()
+    nftBalance.save()
+    
+    //collect the entities of transfer traces
+    // let transferEntity = new TransferTrace(event.transaction.hash.toHex())
+    // transferEntity.from = event.params.from
+    // transferEntity.to = event.params.to
+    // transferEntity.timestamp = event.block.timestamp
+    // transferEntity.save()
+    
+}
