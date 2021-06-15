38359519a57e6956fe71c64f1a0d696b0c754902
cui <qincuidhu@gmail.com>
2021-06-15 22:42:09 +0200

    generated mappings

---
+++ nft-scanner/src/mainnet/EtheremonMonsterMapping.ts
@@ -0,0 +1,28 @@
+import { log, BigInt } from '@graphprotocol/graph-ts';
+import { TokenHolder } from "../../generated/schema";
+import {
+  Transfer
+} from '../../generated/EtheremonMonster/EtheremonMonster';
+
+export function handleTransfer(event: Transfer): void {
+  let id = event.address.toHex() + '#' + event.params._tokenId.toHex();
+
+  log.info('Handle transfer: id: {}, address: {}, tokenId: {}', [id, event.address.toString(), event.params._tokenId.toString()]);
+
+  let entity = TokenHolder.load(id);
+  if (entity == null) {
+    entity = new TokenHolder(id);
+  }
+
+  entity.contract = event.address;
+  entity.tokenId = event.params._tokenId;
+  entity.owner = event.params._to;
+
+  entity.save();
+  
+  // let contract = ERC721.bind(event.address)
+  // let tokenBalance = new TokenBalance(event.params._to.toHex());
+  // tokenBalance.amount = contract.balanceOf(event.params._to)
+  // tokenBalance.totalSupply = contract.totalSupply()
+  // tokenBalance.save()
+}
